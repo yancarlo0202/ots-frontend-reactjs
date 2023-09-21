6ae994146dec005504b950dad632228fcc806020
yancarlo0202 <139280488+yancarlo0202@users.noreply.github.com>
2023-09-21 01:31:39 +0000

    construção do card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 
 function App() {
-  return (
+   return (
     <>
-      <div>
-         App
+      <div className = "card">
+         <h2>Nerd</h2>
+         <img src="https://static.simpsonswiki.com/images/thumb/6/6a/Nerd_%28Robot_Chicken%29.png/375px-Nerd_%28Robot_Chicken%29.png"></img>
       </div>
      
     </>
