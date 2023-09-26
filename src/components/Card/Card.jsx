0d2c33b11ee06bd2e547a90743d4791c2faf226f
yancarlo0202 <139280488+yancarlo0202@users.noreply.github.com>
2023-09-26 00:07:07 +0000

    criando pasta components

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import "./Card.css"
+
+export default function Card(){
+    return <div className = "card">
+    <h2>Nerd</h2>
+
+     <div className="tags">
+       <div className="tag">Status: Virgem</div>
+       <div className="tag">Espécie: Nerd</div>
+     </div>
+
+
+    <img src="https://static.simpsonswiki.com/images/thumb/6/6a/Nerd_%28Robot_Chicken%29.png/375px-Nerd_%28Robot_Chicken%29.png"></img>
+}
