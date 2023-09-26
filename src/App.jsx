0187c15f600f5e43527beb8b691b2e2924403820
yancarlo0202 <139280488+yancarlo0202@users.noreply.github.com>
2023-09-26 01:22:03 +0000

    criando item 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,31 @@ function App() {
     nome: 'Nerd',
     imagemUrl: 'https://static.simpsonswiki.com/images/thumb/6/6a/Nerd_%28Robot_Chicken%29.png/375px-Nerd_%28Robot_Chicken%29.png'
   }
+
+  const item2 = {
+    nome: 'Frango Rôbo',
+    imagemUrl: ''
+  }
    return (
     <>
-      <Card item = {item1} />
-      <div></div>
-     
+      <Card item={item1} />
+      <Card item={item2} />
     </>
   )
+}
+
+const itens = [item1, item2]
+
+const itens = [item1, item2]
+
+return (
+  <>
+    {itens.map(function (item) {
+      return <Card item={item}/>
+    })}
+  </>
+)
+
 }
 
 export default App
