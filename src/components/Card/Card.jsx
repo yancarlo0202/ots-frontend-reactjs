747286b25799ae0dd8e2dcf059023905c58ff497
yancarlo0202 <139280488+yancarlo0202@users.noreply.github.com>
2023-09-27 22:51:06 +0000

    trocanod link do img para itens

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -13,7 +13,7 @@ export default function Card(props){
      </div>
 
 
-    <img src="https://static.simpsonswiki.com/images/thumb/6/6a/Nerd_%28Robot_Chicken%29.png/375px-Nerd_%28Robot_Chicken%29.png"></img> 
+    <img src={item.imagemUrl}></img> 
     
     </div>
     
